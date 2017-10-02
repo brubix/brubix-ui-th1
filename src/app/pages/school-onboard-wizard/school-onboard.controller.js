27ccdb43da0ab40f4559c2467551bbcef0e6c479
sanjeev.singh <sanjeev.singh1@olacabs.com>
2017-10-02 21:09:43 +0530

    Initial Commit

--- src/app/pages/school-onboard-wizard/school-onboard.controller.js
+++ src/app/pages/school-onboard-wizard/school-onboard.controller.js
@@ -6,10 +6,13 @@
     .controller('SchoolOnBoardWizardController', SchoolOnBoardWizardController);
 
   /** @ngInject */
-  function SchoolOnBoardWizardController($log) {
-    var vm = this;
+  function SchoolOnBoardWizardController($log, $resource, $scope) {
+    var vm = $scope;
     vm.activeStep = 0;
     vm.user = {};
+    $resource('http://www.filltext.com/?rows=1&pretty=true&name={firstName}&userName={firstName}&password=[%22password%22]&repeatPassword=[%22password%22]&aboutYou={lorem|100}&address={addressObject}&phoneNumber={phone|format}&emailId={email}&faceBookURL=[%22http://facebook.com%22]&acceptTermCondition=[true]&sendNewsLetter=[true]').query().$promise.then(function(kyc) {
+        vm.kyc = kyc[0];
+    });
 
     vm.submit = function(){
       $log.log(vm.user);
